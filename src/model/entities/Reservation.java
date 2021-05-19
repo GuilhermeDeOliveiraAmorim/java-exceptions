57abe6c0828011cf23bdf736e4dcaf68a9cdabce
GuilhermeDeOliveiraAmorim <guilherme.o.a.ufal@gmail.com>
2021-05-19 16:13:35 -0300

    Trabalgo com exception personalizadas

---
+++ src/model/entities/Reservation.java
@@ -0,0 +1,64 @@
+package model.entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+import model.exceptions.DomainException;
+
+public class Reservation {
+	
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyyy");
+	
+	private Integer roomNumber;
+	private Date checkIn;
+	private Date checkOut;
+	
+	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) throws DomainException {
+		if(!checkOut.after(checkIn)) {
+			throw new DomainException("Dates of future?");
+		}
+		this.roomNumber = roomNumber;
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+	}
+
+	public Integer getRoomNumber() {
+		return roomNumber;
+	}
+
+	public void setRoomNumber(Integer roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+
+	public Date getCheckIn() {
+		return checkIn;
+	}
+
+	public Date getCheckOut() {
+		return checkOut;
+	}
+	
+	public long duration() {
+		long diff = checkOut.getTime() - checkIn.getTime();
+		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+	}
+	
+	public void updateDates(Date checkIn, Date checkOut) throws DomainException {
+		Date now = new Date();
+		if(checkIn.before(now) || checkOut.before(now)) {
+			throw new DomainException("Dates of past?");
+		}
+		if(!checkOut.after(checkIn)) {
+			throw new DomainException("Dates of future?");
+		}
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+	}
+
+	@Override
+	public String toString() {
+		return "Reservation [roomNumber=" + roomNumber + ", checkIn=" + sdf.format(checkIn) + ", checkOut=" + sdf.format(checkOut) + ", nights=" + duration() + "]";
+	} 
+
+}
